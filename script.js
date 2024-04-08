7e73fe22d517895c8fe56d7a1fc79c21dc305b88
winati17 <winatimn@gmail.com>
2024-04-08 19:09:08 +0800

    initial commit, basic req submission dicoding

---
+++ script.js
@@ -0,0 +1,8 @@
+let typed = new Typed("#text", {
+  strings: ['Cyber Security', 'Robotic', 'Data Science', 'Software Engineering'],
+  typeSpeed: 60,
+  backSpeed: 60,
+  backDelay: 2000,
+  loop:true
+});
+
